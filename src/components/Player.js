c5acc6735123b557b5002334b6d4282446241566
wielki.borsuk <wielki.borsuk@gmail.com>
2020-10-23 12:25:17 +0200

    add awesome icons, add volume and speed control

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import './Player.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStepBackward, faStepForward, faPlay, faPause, faVolumeUp, faVolumeMute, faTachometerAlt } from '@fortawesome/free-solid-svg-icons'
 
 class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentTime: 0,
-      duration: 0
+      duration: 0,
+      volume: 1,
+      muted: false,
+      speed: 1
     };
     this.setTime = this.setTime.bind(this);
+    this.setVolume = this.setVolume.bind(this);
+    this.setSpeed = this.setSpeed.bind(this);
   }
 
   render() {
+    const playIcon = this.props.current.playing ? faPause : faPlay;
+    const muteIcon = this.state.muted ? faVolumeMute : faVolumeUp;
+
     return (
       <div className="Player">
+        <audio ref={ref => this.player = ref} src={this.props.current.url} muted={this.state.muted}></audio>
+
         <input type="range" min="0" max={this.state.duration} value={this.state.currentTime} onChange={this.setTime} step="1" />
-        <audio ref={ref => this.player = ref} src={this.props.current.url}></audio>
-        <button onClick={this.toggle}>prev</button>
-        <button onClick={() => this.props.setPlaying(!this.props.current.playing)}>{this.props.current.playing ? 'pause' : 'play'}</button>
-        <button onClick={this.toggle}>next</button>
+        <button onClick={this.toggle}><FontAwesomeIcon icon={faStepBackward} /></button>
+        <button onClick={() => this.props.setPlaying(!this.props.current.playing)}><FontAwesomeIcon icon={playIcon} /></button>
+        <button onClick={this.toggle}><FontAwesomeIcon icon={faStepForward} /></button>
         <span>{this.props.song.name}</span>
+
+        <button onClick={() => this.setState((state) => ({muted: !state.muted}))}><FontAwesomeIcon icon={muteIcon} /></button>
+        <input type="range" min="0" max="1" value={this.state.volume} onChange={this.setVolume} step="0.1" title={this.state.volume} />
+        <button disabled><FontAwesomeIcon icon={faTachometerAlt} /></button>
+        <input type="range" min="0.5" max="2" value={this.state.speed} onChange={this.setSpeed} step="0.1" title={this.state.speed} />
         <span>{this.formatTime(this.state.currentTime)}/{this.formatTime(this.state.duration)}</span>
       </div>
       )
@@ -35,6 +51,13 @@ class Player extends React.Component {
         this.player.play();
       }
     }
+
+    if (prevState.speed !== this.state.speed) {
+      this.player.playbackRate = this.state.speed;
+    }
+    if (prevState.volume !== this.state.volume) {
+      this.player.volume = this.state.volume;
+    }
   }
 
   componentDidMount() {
@@ -49,6 +72,7 @@ class Player extends React.Component {
         currentTime: e.target.currentTime || 0,
         duration: e.target.duration || 0
       });
+      this.player.playbackRate = this.state.speed;
     });
     this.player.addEventListener('play', e => {
       this.props.setPlaying(true);
@@ -68,6 +92,14 @@ class Player extends React.Component {
   setTime(e) {
     this.setState({currentTime: e.target.value});
     this.player.currentTime = e.target.value;
+  }
+
+  setVolume(e) {
+    this.setState({volume: e.target.value});
+  }
+
+  setSpeed(e) {
+    this.setState({speed: e.target.value});
   }
 
   formatTime(time) {
