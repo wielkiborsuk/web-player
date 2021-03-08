02d0aa6086488a284313c9a30fbdc275abbc733b
wielki.borsuk <wielki.borsuk@gmail.com>
2021-03-08 15:23:55 +0100

    add repeat toggle and state handling

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { play, pause, setVolume, setSpeed, setMuted, setDuration, setCurrentTime, toggleShowSpeed, toggleShowBookmarks, saveBookmark, loadBookmark } from '../state/playerSlice';
-import { next, previous, shuffle } from '../state/playlistSlice';
+import { next, previous, shuffle, toggleRepeat } from '../state/playlistSlice';
 import { fetchSource, showSettings } from '../state/sourcesSlice';
 import './Player.css';
 import { formatTime } from './helpers';
 import { ButtonGroup, Button, Slider, Box } from '@material-ui/core';
-import { SkipNext, SkipPrevious, PlayArrow, Pause, Speed, VolumeOff, VolumeUp, Shuffle, Refresh, Bookmarks, CloudDownload, CloudUpload, Settings } from '@material-ui/icons';
+import { SkipNext, SkipPrevious, PlayArrow, Pause, Speed, VolumeOff, VolumeUp, Shuffle, Repeat, Refresh, Bookmarks, CloudDownload, CloudUpload, Settings } from '@material-ui/icons';
 
 export default function Player(props) {
   const dispatch = useDispatch();
   const song = useSelector(s => s.playlist.song);
   const playback = useSelector(s => s.player);
+  const repeat = useSelector(s => s.playlist.repeat) || false;
   const player = useRef();
   const prevTime = useRef(0);
 
@@ -84,6 +85,7 @@ export default function Player(props) {
         <Button onClick={() => dispatch(playAction())}>{playIcon}</Button>
         <Button onClick={() => dispatch(next())}><SkipNext /></Button>
         <Button onClick={() => dispatch(shuffle())}><Shuffle /></Button>
+        <Button onClick={() => dispatch(toggleRepeat())} classes={{ root: repeat?"active":""}}><Repeat /></Button>
       </ButtonGroup>
 
       <ButtonGroup size="small" id="meta-controls">
@@ -92,13 +94,13 @@ export default function Player(props) {
         <Button className={'slider-small'}>
           <Slider min={0} max={1} value={volume} onChange={(e, value) => dispatch(setVolume(value))} step={0.1} title={volume} />
         </Button>
-        <Button onClick={() => dispatch(toggleShowSpeed())} variant="outlined" size="small"><Speed /></Button>
+        <Button onClick={() => dispatch(toggleShowSpeed())} classes={{ root: showSpeed?"active":""}}><Speed /></Button>
         {showSpeed &&
         <Button className={'slider-small'}>
           <Slider min={0.5} max={2} value={speed} onChange={(e, value) => dispatch(setSpeed(value))} step={0.1} title={speed} />
         </Button>
         }
-        <Button onClick={() => dispatch(toggleShowBookmarks())} ><Bookmarks /></Button>
+        <Button onClick={() => dispatch(toggleShowBookmarks())} classes={{ root: showBookmarks?"active":""}} ><Bookmarks /></Button>
         {showBookmarks && <Button onClick={() => dispatch(saveBookmark())}><CloudDownload /></Button>}
         {showBookmarks && <Button onClick={() => dispatch(loadBookmark())}><CloudUpload /></Button>}
         <Button onClick={() => dispatch(showSettings())} ><Settings /></Button>
--- src/state/playlistSlice.js
+++ src/state/playlistSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { saveState, loadState } from './helpers';
 
-const initialState = loadState('playlistState', { list: {}, song: {} });
+const initialState = loadState('playlistState', {
+  list: {},
+  song: {},
+  repeat: false
+});
 
 if (initialState.song.name) {
   document.title = initialState.song.name;
@@ -22,24 +26,32 @@ const playlistSlice = createSlice({
     },
     next(state) {
       const index = state.list.files.findIndex(item => item.name === state.song.name);
-      state.song = state.list.files[(index+1) % state.list.files.length];
-      saveState('playlistState', state);
-      document.title = state.song.name;
+      if (state.repeat || index < state.list.files.length - 1) {
+        state.song = state.list.files[(index+1) % state.list.files.length];
+        saveState('playlistState', state);
+        document.title = state.song.name;
+      }
     },
     previous(state) {
       const listLength = state.list.files.length;
       const index = state.list.files.findIndex(item => item.name === state.song.name);
-      state.song = state.list.files[(index+listLength-1) % listLength];
-      saveState('playlistState', state);
-      document.title = state.song.name;
+      if (state.repeat || index > 0) {
+        state.song = state.list.files[(index+listLength-1) % listLength];
+        saveState('playlistState', state);
+        document.title = state.song.name;
+      }
     },
     shuffle(state) {
       if (state.list && state.list.files) {
         state.list.files = state.list.files.sort(() => Math.random() - 0.5);
       }
+    },
+    toggleRepeat(state) {
+      state.repeat = !state.repeat;
+      saveState('playlistState', state);
     }
   }
 });
 
-export const { setCurrentSong, setCurrentList, next, previous, shuffle } = playlistSlice.actions;
+export const { setCurrentSong, setCurrentList, next, previous, shuffle, toggleRepeat } = playlistSlice.actions;
 export default playlistSlice.reducer;
